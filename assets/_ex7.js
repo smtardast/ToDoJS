c6c09bd1a287708a00ad9c6fd3e91cdd11b52f6e
smtardast <smtardast@gmail.com>
2018-11-21 14:37:52 +0100

    EVERYTHING except enter

--- assets/_ex7.js
+++ assets/_ex7.js
@@ -20,9 +20,6 @@ let newList = [];
 
 
 
-// let doneIcon = ;
-
-// let myElement = document.
 
 
 //define compteur
@@ -37,98 +34,69 @@ let count = 0;
 
 
 //making a function to add elements aka the list elements
-function addListElement() {
+function addListElement(event) {
 
+    if (favInput.value == "") {
 
-    // create li with a span and icons, and with the value og your input in it
+        alert("write summin bih");
 
-    let skrivet = favInput.value;
-    let newTach = document.createElement("li");
-    newTach.classList.add("list-group-item", "inDaList");
-
-    newTach.innerHTML = '<span class="hi" contenteditable="false">' + skrivet + '</span> <i class="float-right fas fa-pen text-info modifyIcon m-1" ></i><i class="float-right fas fa-times text-danger removeIcon m-1"></i><i class="float-right fas fa-check text-success doneIcon m-1"></i>';
-    dasList.appendChild(newTach);
-
-    //nollställ input
-    favInput.value = null;
-
-    //put all created li in a list
-    newList = document.querySelectorAll(".inDaList");
-    console.log(newList);
-
-    // for (let i = 0; i < newList.length; i++) {
-
-    //     console.log("babe");
-
-    //     newList[i].querySelector(".removeIcon").addEventListener("click", () => {
-    //         dasList.removeChild(nth-child(i))
-    //     })
-
-    // }
-
-    //add possibility of done for every li element
-
-    for (let i = 0; i < newList.length; i++) {
-
-        console.log("babe");
-
-        
-        if (newList[i].classList == "done") {
-            newList[i].querySelector(".doneIcon").addEventListener("click", () => {
-                newList[i].classList.remove("done", "bg-success");
-            })
-            console.log("remove me");
-        } else {
-            newList[i].querySelector(".doneIcon").addEventListener("click", () => {
-                newList[i].classList.add("done", "bg-success");
-            })
-            console.log("add me");
-        }
+    } else {
 
 
 
+        // create li with a span and icons, and with the value og your input in it
+
+
+        let newTach = document.createElement("li");
+        let skrivet = favInput.value;
+        newTach.classList.add("list-group-item", "inDaList");
+
+        newTach.innerHTML = '<span class="hi" contenteditable="false">' + skrivet + '</span> <i class="float-right fas fa-pen text-info modifyIcon m-1" ></i><i class="float-right fas fa-times text-danger removeIcon m-1"></i><i class="float-right fas fa-check text-success doneIcon m-1"></i>';
+        dasList.appendChild(newTach);
+
+        let btndone = document.querySelectorAll(".doneIcon");
+        let btnsupp = document.querySelectorAll(".removeIcon");
+        let btnmod = document.querySelectorAll(".modifyIcon");
+        //nollställ input
+        favInput.value = null;
+
+        //put all created li in a list
+        newList = document.querySelectorAll(".inDaList");
+        console.log(newList);
+
+
+
+
+        btndone.forEach(element => {
+            element.addEventListener("click", validateMe)
+            console.log(element);
+        });
+
+
+        btnsupp.forEach(element => {
+            element.addEventListener("click", crossMe)
+        });
+
+        btnmod.forEach(qq => {
+            qq.addEventListener("click", modifyMe)
+        })
     }
-
-    function crossMe() {
-        // make variable for icon. event.target to take just this one. then get parent thru icon!
-        //newTach.parentNode.removeChild(newTach);
-
-
-        dasList.removeChild(event.target.parentElement);
-    }
-
-    function editMe() {
-        // for (let i = 0; i < newList.length; i++) {
-        if (document.querySelector(".hi").contentEditable == "false") {
-            document.querySelector(".hi").contentEditable = "true";
-
-        } else {
-            document.querySelector(".hi").contentEditable = "false";
-
-        }
-        // }
-    }
-
-    // document.querySelector(".doneIcon").addEventListener("click", tickMe);
-
-    let btnsupp = document.querySelectorAll(".removeIcon");
-
-    btnsupp.forEach(element => {
-        element.addEventListener("click", crossMe)
-    });
-
-    document.querySelectorAll(".modifyIcon").addEventListener("click", editMe);
-
-
-
-
 }
+
 
 // make button after input able to create li
 
 addME.addEventListener("click", addListElement);
 
+favInput.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        addListElement;
+    }
+});
+
 //filters 
+
 
 function filterDone() {
     for (let i = 0; i < newList.length; i++) {
@@ -165,6 +133,38 @@ function filterTodo() {
         }
     }
 }
+
+//icons doing things
+
+function validateMe(event) {
+    event.target.parentElement.classList.toggle("bg-success")
+}
+
+function crossMe(event) {
+    dasList.removeChild(event.target.parentElement);
+}
+
+function modifyMe(event) {
+    if (event.target.parentElement.firstChild.contentEditable == "true") {
+
+
+        event.target.parentElement.firstChild.contentEditable = "false";
+
+
+        console.log("stop editing");
+
+    } else {
+
+        event.target.parentElement.firstChild.contentEditable = "true";
+
+
+        console.log("editing");
+
+
+    }
+
+}
+
 
 allBtn.addEventListener("click", filterAll);
 doneBtn.addEventListener("click", filterDone);
